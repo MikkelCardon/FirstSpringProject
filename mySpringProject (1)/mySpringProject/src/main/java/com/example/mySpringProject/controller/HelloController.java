1192dd7b97717351a39b4d14c875afc6e46a899a
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-01-26 16:16:17 +0100

    Test af @GetMapping og @RequestMapping

--- mySpringProject (1)/mySpringProject/src/main/java/com/example/mySpringProject/controller/HelloController.java
+++ mySpringProject (1)/mySpringProject/src/main/java/com/example/mySpringProject/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.mySpringProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Main")
 public class HelloController {
 
     @GetMapping("/helloworld")
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    @GetMapping("/{text}")
+    public String addText(@PathVariable String text){
+        return text;
     }
 }
